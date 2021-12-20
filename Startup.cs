10bad00db3aec029004205467fa010bd194eb9c5
dortiz8 <53200770+dortiz8@users.noreply.github.com>
2021-12-20 14:54:36 -0500

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBills.Startup))]
+namespace MyBills
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
