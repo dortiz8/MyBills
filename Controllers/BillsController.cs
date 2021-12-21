ae6d907529158ab251144d86d43af6ce643334c2
dortiz8 <53200770+dortiz8@users.noreply.github.com>
2021-12-21 09:47:53 -0500

    Added Bills Controller and View

---
+++ Controllers/BillsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBills.Controllers
+{
+    public class BillsController : Controller
+    {
+        // GET: Bills
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
